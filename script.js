b39217a46afb309d9d5cfbff48965325c43d4016
AusPrinzip <cdmdb@hotmail.com>
2024-05-01 13:52:31 -0600

    new files. navbar

--- script.js
+++ script.js
@@ -14,25 +14,21 @@ setInterval(async() => {
 	let cardId = counter % 3;
 	for (let i = 0; i < 3; i++) {
 		const card = document.getElementById(`card-${i}`)
-
 		// first we do fade out
 
 		if (i !== cardId && card.style.display == "block") {
-			// card.style.display = "block"
 			card.classList.remove("animate__slideInRight")
 			// card.classList.add("animate__slideOutLeft")
 			card.classList.add("animate__fadeOut")
 			await wait(1050)
 			card.style.display = "none"
+
 		} 
 
 	}
 
-
-
 	for (let i = 0; i < 3; i++) {
 		const card = document.getElementById(`card-${i}`)
-
 		// then fade in
 
 		if (i == cardId) {
@@ -40,6 +36,17 @@ setInterval(async() => {
 			card.classList.remove("animate__fadeOut")
 			card.classList.add("animate__slideInRight")
 		} 
+	}
+
+	for (let i = 0; i < 3; i++) {
+		const icon = document.getElementById(`icon-${i}`)
+		// then fade in
+
+		if (i == cardId) {
+			icon.style.opacity = 1;
+		} else {
+			icon.style.opacity = 0.5;
+		}
 	}
 
 	counter++
